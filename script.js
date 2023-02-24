7bcddec52e4a288460ef7041fa1ec3b307c36e6b
twdinn <twdinn@gmail.com>
2023-02-24 16:01:40 -0330

    drawing-app

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ let color = "black";
 let x;
 let y;
 
+const drawCircle = (x, y) => {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+const drawLine = (x1, y1, x2, y2) => {
+  ctx.beginPath;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+};
+
+const updateSizeOnScreen = () => {
+  sizeElement.innerHTML = size;
+};
+
 canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
   x = e.offsetX;
@@ -64,23 +84,3 @@ colorElement.addEventListener("change", (e) => {
 clear.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
-
-const updateSizeOnScreen = () => {
-  sizeElement.innerHTML = size;
-};
-
-const drawCircle = (x, y) => {
-  ctx.beginPath();
-  ctx.arc(x, y, size, 0, Math.PI * 2);
-  ctx.fillStyle = color;
-  ctx.fill();
-};
-
-const drawLine = (x1, y1, x2, y2) => {
-  ctx.beginPath;
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = size * 2;
-  ctx.stroke();
-};
